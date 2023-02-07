1df88680dfb3fae1785f27cd6d1fa3b1371cc6e7
BerylKanali <50364045+BerylKanali@users.noreply.github.com>
2023-02-07 18:00:53 +0300

    Update Trello_Sort.R

--- inst/script/Trello_Sort.R
+++ inst/script/Trello_Sort.R
@@ -1,53 +1,15 @@
 library(BiocPkgTools)
 
 # Import data and divide the url to extract package names in one column
-data= read.delim("https://trello.com/1/cards/61d9de5dffd84647db58edb7/attachments/61d9de7a6b634f8b08261871/download/rnw-files.txt", header = FALSE, sep = "/", dec = " ")
-data
-
-
-colnames(data)# check columns
-data$V1 #check the packages column
+filepath <- "https://trello.com/1/cards/61d9de5dffd84647db58edb7/attachments/61d9de7a6b634f8b08261871/download/rnw-files.txt"
+vignettes <- read.delim(filepath, header = FALSE, sep = "/", dec = " ")
+vignettes
 
 # Extract unique packages so that a package name appears only once
-packages=unique(data$V1)# Alternative method = unique(data[c("V1")])
-packages
-# Alternative method = unique(data[c("V1")])
+packagenames=unique(vignettes$V1)
+packagenames
 
-str(packages)
-typeof(packages)# Checking for data typeata type
-length(packages)# Number of variables(packages)
-print(packages[1])
-
-                  
-# Checking if we can get statistics for the packages
-pkgDownloadStats(
-  packages[1],
-  pkgType = c("software", "data-experiment", "workflows", "data-annotation"),
-  years = format(Sys.time(), "%Y")
-)
-
-# Check rank of the first package in the list
-PckgRank=pkgDownloadRank(
-  packages[1],
-  pkgType = c("software", "data-experiment", "workflows", "data-annotation"),
-  version = "3.17"
-)
-
-attributes(PckgRank)
-attributes(PckgRank[1])
-
-# Iterating through each package to get it's rank
-for(i in packages){
-  Rank=pkgDownloadRank(
-    i,
-    pkgType = c("software", "data-experiment", "workflows", "data-annotation"),
-    version = "3.17"
-  )
-  # Alternative method = unique(my[c("V1")])print(Rank)
-  Package_Rank=print(paste(i, Rank))
-}
-
-
+# Getting packages that are in version 3.17
 pckgs <- biocPkgList(
   version = "3.17",
   repo = "BioCsoft",
@@ -56,35 +18,48 @@ pckgs <- biocPkgList(
 
 pckgs
 
+# creating a list of packages from the url that are in version 3.17
 pckgsnew <- list()
-for(i in packages){
+for(i in packagenames){
   if( i %in% pckgs$Package)
-     pckgsnew <- append(pckgsnew,i)
+    pckgsnew <- append(pckgsnew,i)
 }
 
 pckgsnew
 
-length(pckgsnew)
+# Getting list of packages maintained by bioconductor
+maintainer <- biocMaintained(
+  version = BiocManager::version(),
+  main = "maintainer@bioconductor\\.org"
+)
+
+maintainer
+
+coremaintained <- list()
+coremaintained<- list(maintainer$Package)
+coremaintained
+
 
 pkgsList <- list()
-for(i in head(pckgsnew)){
-  Rank=pkgDownloadRank(
-    i,
-    pkgType = c("software", "data-experiment", "workflows", "data-annotation"),
-    version = "3.17"
-  )
-  # Alternative method = unique(my[c("V1")])print(Rank)
-  pkgsList <- append(pkgsList, list(package = i, rank = Rank))
+threshold <- 40
+# creates rank
+for(i in 1:length(head(pckgsnew))){
+  rank <- pkgDownloadRank(i, version = "3.17")
+  pkgsList[[i]] <- list(package = pckgsnew[[i]],
+                        rank = rank[[1]],
+                        priority = ifelse(rank[[1]] > threshold, "High","Low"),
+                        status = ifelse(i %in% coremaintained, "To do","Contact Maintainer"))
 }
 
 pkgsList
 
-head(pckgsnew)
 
-head(pkgsList)
-colnames(pkgsList)
-names(pkgsList)
+install.packages("openxlsx")
+library(openxlsx)
 
+# Convert list to dataframe
+dataframe <- as.data.frame(do.call(rbind, pkgsList))
+dataframe
 
-pkgsList$Priority = ifelse(pkgsList$rank > 40,"High","Low")
-print(pkgsList)
+# Output the dataframe as an excel file
+write.xlsx(dataframe, "sweave2rmd.xlsx", col_names= TRUE)
