9670b85d66a0935cd2fe2790149fff7d26e0a85f
BerylKanali <50364045+BerylKanali@users.noreply.github.com>
2023-02-14 11:41:55 +0300

    Update Trello_Sort.R

--- inst/script/Trello_Sort.R
+++ inst/script/Trello_Sort.R
@@ -5,7 +5,7 @@
 
 # Using the script: input the list of vignettes, the script first extracts the
 # package names then checks if the package is in the current bioconductor
-# version (the current version is 3.17), outputs their rank and from the rank
+# version (the current version is 3.17) , outputs their rank and from the rank
 # assigns a priority based on the set threshold then checks for the maintainer
 # and assigns the package a status based on who the maintainer is.
 
@@ -23,26 +23,26 @@ threshold <- 40
 version <- "3.17"
 repo <- "BioCsoft"
 output_file <- "sweave2rmd.xlsx"
+filepath <- "https://trello.com/1/cards/61d9de5dffd84647db58edb7/attachments/61d9de7a6b634f8b08261871/download/rnw-files.txt"
 
 # Import data and divide the url to extract package names in one column
-filepath <- "https://trello.com/1/cards/61d9de5dffd84647db58edb7/attachments/61d9de7a6b634f8b08261871/download/rnw-files.txt"
 vignettes <- read.delim(filepath, header = FALSE, sep = "/", dec = " ")
 
 # Extract unique packages so that a package name appears only once
 packageNames <- unique(vignettes$V1)
 
-# Getting packages that are in bioconductor version 3.17
+# Get packages in specified Bioconductor version
 biocPckgs <- biocPkgList(
   version,
   repo,
   addBiocViewParents = TRUE
 )
 
-# creating a list of packages from the vignette list that are in version 3.17
+# Create list of packages from vignette list in specified version
 pckgs <- list()
-for(i in packagenames){
+for(i in packageNames){
   if( i %in% biocPckgs$Package)
-    pckgs <- append(pckgs,i)
+    pckgs <- append(pckgs, i)
 }
 
 # Getting list of packages maintained by bioconductor
@@ -51,13 +51,13 @@ coreMaintained <- list(biocMaintained()$Package)
 # For every package, their rank, priotrity and status is computed. This is the
 # output that we want from the script which will help us categorize the
 # packages.
-
+pkgsList <- list()
 for(i in 1:length(pckgs)){
-  rank <- pkgDownloadRank(i,"software" ,version)
+  rank <- pkgDownloadRank(i, "software", version)
   pkgsList[[i]] <- list(package = pckgs[[i]],
                         rank = rank[[1]],
-                        priority = ifelse(rank[[1]] > threshold, "High","Low"),
-                        status = ifelse(i %in% coremaintained, "To do","Contact Maintainer"))
+                        priority = ifelse(rank[[1]] > threshold, "High", "Low"),
+                        status = ifelse(i %in% coreMaintained, "To do", "Contact Maintainer"))
   
 }
 
@@ -66,4 +66,3 @@ dataframe <- as.data.frame(do.call(rbind, pkgsList))
 
 # Final output as an excel file
 write.xlsx(dataframe, output_file, col_names= TRUE)
-
